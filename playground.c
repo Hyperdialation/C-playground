9e324ab0e92299033f1467d88c2bb09af1ff2929
Hyperdialation <146753292+Hyperdialation@users.noreply.github.com>
2025-01-01 21:31:24 +0530

    Update playground.c

--- playground.c
+++ playground.c
@@ -1,21 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void  main()
-{
-    int x,n,rem,temp;
-    printf("Enter the input number\n");
-    scanf("%d",&x);
-    n=2;
-    rem=1;
-    while(n<x || rem<0)
-    {
-        temp=x/n;
-        rem=temp%10;
-        n=n+1
-        if(rem==0)
-    }
-    
+int main({
     
 
-}
+})
+
